a688a7fc79e47a8892822866fe9acbde6f9043c5
MrGlox <contact@morgan-leroux.com>
2024-06-06 14:42:43 +0200

    update shaders

--- components/webgl/Galaxy/Galaxy.tsx
+++ components/webgl/Galaxy/Galaxy.tsx
@@ -3,8 +3,38 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
 import * as THREE from "three";
 
-import vertexShader from "!!raw-loader!./vertexShader.glsl";
-import fragmentShader from "!!raw-loader!./fragmentShader.glsl";
+import vertexShader from "./vertexShader.glsl";
+import fragmentShader from "./fragmentShader.glsl";
+
+// const fragmentShader = `
+// varying float vDistance;
+
+// void main() {
+//     vec3 color = vec3(0.34, 0.53, 0.96);
+//     float strength = distance(gl_PointCoord, vec2(0.5));
+//     strength = 1.0 - strength;
+//     strength = pow(strength, 3.0);
+
+//     color = mix(color, vec3(0.97, 0.70, 0.45), vDistance * 0.5);
+//     color = mix(vec3(0.0), color, strength);
+//     gl_FragColor = vec4(color, strength);
+// }
+// `;
+
+// const vertexShader = `
+// varying float vDistance;
+
+// void main() {
+//     vec3 color = vec3(0.34, 0.53, 0.96);
+//     float strength = distance(gl_PointCoord, vec2(0.5));
+//     strength = 1.0 - strength;
+//     strength = pow(strength, 3.0);
+
+//     color = mix(color, vec3(0.97, 0.70, 0.45), vDistance * 0.5);
+//     color = mix(vec3(0.0), color, strength);
+//     gl_FragColor = vec4(color, strength);
+// }
+// `;
 
 const CustomGeometryParticles = (props) => {
   const { count } = props;
