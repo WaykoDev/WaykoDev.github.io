95268cd1fcc2381118f90174199424fc0ba883f6
MrGlox <contact@morgan-leroux.com>
2024-06-06 12:14:46 +0200

    galaxy only on home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import Galaxy from "../components/webgl/Galaxy/Galaxy";
+// import Galaxy from "../components/webgl/Galaxy/Galaxy";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Galaxy />
+      {/* <Galaxy /> */}
       <Component {...pageProps} />
     </>
   );
